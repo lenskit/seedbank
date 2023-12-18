7a270253a8f9bf57f322f18c0b475ab4abad4ddf
Michael Ekstrand <mdekstrand@drexel.edu>
2023-12-18 16:21:26 -0500

    add tests for make_key

---
+++ tests/test_process_seed.py
@@ -0,0 +1,36 @@
+"""
+Tests for processing seed material.
+"""
+
+import numpy as np
+
+from seedbank._keys import make_key
+
+
+def test_make_key_int():
+    key = make_key(42)
+    assert key == 42
+
+
+def test_make_key_npint():
+    key = make_key(np.int32(42))
+    assert key == 42
+
+
+def test_make_key_str():
+    key = make_key("hello, world")
+    assert isinstance(key, np.ndarray)
+    assert len(key) >= 4
+
+
+def test_make_key_bytes():
+    key = make_key(b"hello, world")
+    assert isinstance(key, np.ndarray)
+    assert len(key) >= 4
+
+
+def test_make_key_ndarray():
+    src = np.random.randint(500, size=32)
+    key = make_key(src)
+    assert isinstance(key, np.ndarray)
+    assert all(key == src)
