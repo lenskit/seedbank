7dd3e912b12db11c777b9b150e44505ad24b95be
Michael Ekstrand <md@ekstrandom.net>
2024-05-29 09:30:46 -0400

    add file headers

--- seedbank/jax.py
+++ seedbank/jax.py
@@ -1,3 +1,9 @@
+# This file is part of SeedBank.
+# Copyright (C) 2021-2023 Boise State University
+# Copyright (C) 2023-2024 Drexel University
+# Licensed under the MIT license, see LICENSE.md for details.
+# SPDX-License-Identifier: MIT
+
 """
 JAX support.
 
